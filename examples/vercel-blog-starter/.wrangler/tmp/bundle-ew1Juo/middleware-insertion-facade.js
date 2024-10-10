dc104b6a8d8c60f3d5c4bb050b6bc3ed44a0fad5
shashankboosi <shashank1872@gmail.com>
2024-10-11 04:39:29 +1100

    Add vercel blog starter to examples

---
+++ examples/vercel-blog-starter/.wrangler/tmp/bundle-ew1Juo/middleware-insertion-facade.js
@@ -0,0 +1,11 @@
+				import worker, * as OTHER_EXPORTS from "/Users/shashankreddyboosi/Documents/Projects/opensource/opennextjs-cloudflare/examples/vercel-blog-starter/.worker-next/index.mjs";
+				import * as __MIDDLEWARE_0__ from "/Users/shashankreddyboosi/Documents/Projects/opensource/opennextjs-cloudflare/node_modules/.pnpm/wrangler@3.80.2_@cloudflare+workers-types@4.20240925.0/node_modules/wrangler/templates/middleware/middleware-ensure-req-body-drained.ts";
+import * as __MIDDLEWARE_1__ from "/Users/shashankreddyboosi/Documents/Projects/opensource/opennextjs-cloudflare/node_modules/.pnpm/wrangler@3.80.2_@cloudflare+workers-types@4.20240925.0/node_modules/wrangler/templates/middleware/middleware-miniflare3-json-error.ts";
+
+				export * from "/Users/shashankreddyboosi/Documents/Projects/opensource/opennextjs-cloudflare/examples/vercel-blog-starter/.worker-next/index.mjs";
+
+				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
+					
+					__MIDDLEWARE_0__.default,__MIDDLEWARE_1__.default
+				]
+				export default worker;
